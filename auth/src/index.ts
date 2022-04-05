ea548c91c8424eff38ae96991a4934ba66954494
Abdullah1428 <abdullahcse1428@gmail.com>
2022-04-05 10:44:25 +0200

    big change

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Starting up the auth...');
+  console.log('Starting up the auth...a big change');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
